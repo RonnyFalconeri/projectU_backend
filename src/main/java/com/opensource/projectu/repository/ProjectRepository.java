d0d29379c74a177a9f86285769eb2233a2f3f03c
Ronny Falconeri <ronny.falconeri@prodyna.com>
2022-04-03 12:10:13 +0200

    refactoring

--- src/main/java/com/opensource/projectu/repository/ProjectRepository.java
+++ src/main/java/com/opensource/projectu/repository/ProjectRepository.java
@@ -1,4 +1,4 @@
-package com.opensource.projectu.service;
+package com.opensource.projectu.repository;
 
 import com.opensource.projectu.openapi.model.Project;
 import org.springframework.data.mongodb.repository.MongoRepository;
--- src/main/java/com/opensource/projectu/service/ProjectService.java
+++ src/main/java/com/opensource/projectu/service/ProjectService.java
@@ -1,11 +1,12 @@
 package com.opensource.projectu.service;
 
+import com.opensource.projectu.exception.NotFoundException;
 import com.opensource.projectu.openapi.model.Project;
+import com.opensource.projectu.repository.ProjectRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
-import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -13,39 +14,40 @@ public class ProjectService {
 
     private final ProjectRepository projectRepository;
 
-    public Optional<Project> getProjectById(String id) {
-        return projectRepository.findById(id);
+    public Project getProjectById(String id) {
+        return projectRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Project with id "+ id +" not found."));
     }
 
     public List<Project> getAllProjects() {
         return projectRepository.findAll();
     }
 
-    public void createProject(Project project) {
-        // TODO: check if all necessary fields are set
-        // TODO: preset specific fields like state -> always INITIATED at beginning
-        // TODO: validate fields
-        projectRepository.save(project);
+    public Project createProject(Project project) {
+        project.setId(UUID.randomUUID().toString());
+        return projectRepository.save(project);
     }
 
-    public void updateProject(String id, Project updatedProject) {
-        // TODO: check if all necessary fields are set
-        // TODO: validate fields
+    public Project updateProject(String id, Project newProject) {
+        var currentProject = projectRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Project with id "+ id +" not found."));
 
-        Optional<Project> project = projectRepository.findById(id);
+        var project = copyProjectProperties(newProject, currentProject);
+        return projectRepository.save(project);
+    }
 
-        if(project.isPresent()) {
-            Project projectToUpdate = project.get();
-
-            projectToUpdate.setTitle(updatedProject.getTitle());
-            projectToUpdate.setDescription(updatedProject.getDescription());
-            projectToUpdate.setTasks(updatedProject.getTasks());
-            projectToUpdate.setState(updatedProject.getState());
-            projectToUpdate.setComplexity(updatedProject.getComplexity());
-            projectToUpdate.setEstimatedDurationInHours(updatedProject.getEstimatedDurationInHours());
-
-            projectRepository.save(projectToUpdate);
-        }
+    private Project copyProjectProperties(Project newProject, Project currentProject) {
+        currentProject.setTitle(newProject.getTitle());
+        currentProject.setDescription(newProject.getDescription());
+        currentProject.setTasks(newProject.getTasks());
+        currentProject.setState(newProject.getState());
+        currentProject.setComplexity(newProject.getComplexity());
+        currentProject.setEstimatedDurationInHours(newProject.getEstimatedDurationInHours());
+        currentProject.setExpectedResult(newProject.getExpectedResult());
+        currentProject.setActualResult(newProject.getActualResult());
+        currentProject.setStartedAt(newProject.getStartedAt());
+        currentProject.setFinishedAt(newProject.getFinishedAt());
+        return currentProject;
     }
 
     public void deleteProject(String id) {
